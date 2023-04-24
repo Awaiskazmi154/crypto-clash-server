896cff8f1d2b41e6bfaa89161aa24f27bf6e1fbc
AwaisKazmi154 <awaiskfiverr@gmail.com>
2023-04-24 17:19:18 +0500

    index js set

--- index.js
+++ index.js
@@ -71,13 +71,13 @@ io.on("connection", (socket) => {
   });
 });
 
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
 
-  app.use(express.static(path.join(__dirname, '/client/build')))
+//   app.use(express.static(path.join(__dirname, '/client/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
-}
+// }
 
 // Start the server
 const PORT = process.env.PORT || 5000;
