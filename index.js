4f0fe705be369a06551cb1abb9ec55290cb8ae6a
AwaisKazmi154 <awaiskfiverr@gmail.com>
2023-04-24 07:24:11 +0500

    cors set

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://crypto-clash-server.vercel.app",
     methods: ["GET", "POST"],
   },
 });
